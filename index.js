53517d28d0a4f2ae1f679eb030fc3678b7532fc7
Apple <apple@Apples-MacBook-Pro-38.local>
2019-07-30 13:26:42 -0700

    first code

---
+++ index.js
@@ -0,0 +1,30 @@
+var button = $('.button')
+var header = $('.header')
+var text = $('.text')
+var paragraph = $('.paragraph')
+var hello= $('.hello')
+
+var quotes= ["Happiest girls are the prettiest",
+ "The challenges in life shape you into the person you were meant to be",
+  "Everything that happens has a purpose",
+   "Go get some Ice Cream, you deserve it",
+    "You're beautiful",
+"Laughing burns more calories"]
+
+
+
+button.on("click", inspirational);
+
+function inspirational() {
+  console.log ("hello");
+  console.log(quotes);
+}
+button.on("click", pickQuote);
+
+
+function pickQuote(){
+  var random= Math.random() * 20;
+  var rounded= Math.floor(random);
+  var quote= quotes[rounded];
+  hello.text(quote);
+}
