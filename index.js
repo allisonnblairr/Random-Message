765b4bc4ec8216baf6fe7201bb0ea4a4d95650b2
Apple <apple@Apples-MacBook-Pro-38.local>
2019-07-30 13:44:51 -0700

    updated code

--- index.js
+++ index.js
@@ -4,27 +4,21 @@ var text = $('.text')
 var paragraph = $('.paragraph')
 var hello= $('.hello')
 
-var quotes= ["Happiest girls are the prettiest",
- "The challenges in life shape you into the person you were meant to be",
-  "Everything that happens has a purpose",
-   "Go get some Ice Cream, you deserve it",
-    "You're beautiful",
-"Laughing burns more calories"]
+var quotes = ["Happiest girls are the prettiest", "The challenges in life shape you into the person you were meant to be", "Everything that happens has a purpose", "Go get some Ice Cream, you deserve it", "You're beautiful", "Laughing burns more calories"]
 
 
+// button.on("click", inspirational);
+//
+// function inspirational() {
+//   console.log ("hello");
+//   console.log(quotes);
+// }
 
-button.on("click", inspirational);
-
-function inspirational() {
-  console.log ("hello");
-  console.log(quotes);
-}
 button.on("click", pickQuote);
-
-
-function pickQuote(){
-  var random= Math.random() * 20;
-  var rounded= Math.floor(random);
-  var quote= quotes[rounded];
+function pickQuote() {
+  event.preventDefault();
+  var random = Math.random() * quotes.length;
+  var rounded = Math.floor(random);
+  var quote = quotes[rounded];
   hello.text(quote);
 }
